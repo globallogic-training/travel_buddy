0ba731986a91a2a51d8fa7135d6db5d536c55a31
Nitish Kumar <nitish.kumar4@globallogic.com>
2019-08-13 15:59:46 +0530

    reverted searchactivity function

--- app/src/main/java/com/challenge/travel_buddy/train/trainsearch/view/ui/TrainSearch.java
+++ app/src/main/java/com/challenge/travel_buddy/train/trainsearch/view/ui/TrainSearch.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 
 import com.challenge.travel_buddy.MVVMApplication;
 import com.challenge.travel_buddy.R;
+import com.challenge.travel_buddy.train.trainsearch.di.DaggerTrainSearchActivityComponent;
 import com.challenge.travel_buddy.train.trainsearch.di.TrainSearchActivityComponent;
 import com.challenge.travel_buddy.train.trainsearch.services.model.Available_Status;
 import com.challenge.travel_buddy.train.trainsearch.services.model.Train;
@@ -121,8 +122,8 @@ public class TrainSearch extends AppCompatActivity {
         progressBar.setVisibility(View.VISIBLE);
         trainListProgressBar.setVisibility(View.VISIBLE);
 
-//        TrainSearchActivityComponent trainSearchActivityComponent = DaggerTrainSearchActivityComponent.builder().appComponent(((MVVMApplication) getApplication()).getAppComponent()).build();
-//        trainSearchActivityComponent.inject(this);
+        TrainSearchActivityComponent trainSearchActivityComponent = DaggerTrainSearchActivityComponent.builder().appComponent(((MVVMApplication) getApplication()).getAppComponent()).build();
+        trainSearchActivityComponent.inject(this);
 
         final TrainListViewModel viewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(TrainListViewModel.class);
@@ -151,7 +152,99 @@ public class TrainSearch extends AppCompatActivity {
                 .observe(this, new Observer<List<TrainAvailabilityModel>>() {
                     @Override
                     public void onChanged(List<TrainAvailabilityModel> data) {
-                       setTrainListOnResponse(data,viewModel);
+                        boolean is_best_3A_found = false;
+                        boolean is_best_SL_found = false;
+                        if(data.size() == 30) {
+                            Collections.sort(data, new Comparator<TrainAvailabilityModel>() {
+                                @Override
+                                public int compare(TrainAvailabilityModel o1, TrainAvailabilityModel o2) {
+
+                                    if (o1 == null) {
+                                        return (o2 == null) ? 0 : -1;
+                                    }
+                                    if (o2 == null) {
+                                        return 1;
+                                    }
+
+                                    return Helper.getDateFromString(o1.getDate()).compareTo(Helper.getDateFromString(o2.getDate()));
+                                }
+                            });
+
+                            Iterator<TrainAvailabilityModel> itr = data.iterator();
+                            while(itr.hasNext()){
+                                TrainAvailabilityModel trainmaodel = itr.next();
+
+                                if(trainmaodel == null)
+                                    itr.remove();
+                            }
+                            for(int i=0; i< data.size(); i++){
+                                Map<String, Map<String, Available_Status>> x = data.get(i).getData();
+                                String trainDate = Helper.getDateFromTimeStamp(data.get(i).getDate());
+                                Set<String> keys = x.keySet();
+                                for(String key: keys){
+                                    Map<String, Available_Status> avail = x.get(key);
+                                    if(avail.containsKey("SL|GN")) {
+                                        if (!is_best_SL_found && avail.get("SL|GN") != null && avail.get("SL|GN").getA() != null && avail.get("SL|GN").getA().startsWith("A")) {
+                                            bestWithClassSL.put("SL|GN", avail.get("SL|GN"));
+                                            bestSLDate.put("date",trainDate);
+                                            bestSL.put(key, bestWithClassSL);
+                                            is_best_SL_found = true;
+                                        }
+                                    }
+                                    if(avail.containsKey("3A|GN")){
+                                        if(!is_best_3A_found && avail.get("3A|GN")!=null && avail.get("3A|GN").getA()!=null && avail.get("3A|GN").getA().startsWith("A")){
+                                            bestWithClass3A.put("3A|GN",avail.get("3A|GN"));
+                                            best3A.put(key,bestWithClass3A);
+                                            best3aDate.put("date", trainDate);
+                                            is_best_3A_found = true;
+                                        }
+                                    }
+                                }
+                                Log.d("train", data.toString());
+                            }
+
+                            if(bestSL.size() > 0){
+                                Set<String> keys = bestSL.keySet();
+                                String bestTrainNumberSL = keys.iterator().next() != "" ? keys.iterator().next() : "Not Found";
+                                Available_Status bestSLObj = bestSL.get(keys.iterator().next()).get("SL|GN");
+                                best_SL_trainNumber.setText(bestTrainNumberSL);
+                                best_SL_Availability.setText(bestSLObj.getA());
+                                best_SL_date_lbl.setText(" on ");
+                                best_SL_lbl.setText("SL : ");
+                                best_SL_date.setText(bestSLDate.get("date"));
+                                viewModel.getTrainFromCodeSL(bestTrainNumberSL)
+                                        .observe(TrainSearch.this, new Observer<Train_Number_Search>() {
+                                            @Override
+                                            public void onChanged(Train_Number_Search train_number_search) {
+                                                best_SL_trainName.setText(train_number_search.getN()!=null ? train_number_search.getN() : train_number_search.getCn());
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        });
+                            }else{
+                                progressBar.setVisibility(View.GONE);
+                            }
+
+                            if(best3A.size() > 0){
+                                Set<String> keys = best3A.keySet();
+                                String bestTrainNumber3A = keys.iterator().next() != "" ? keys.iterator().next() : "Not Found";
+                                Available_Status _3AAvail = best3A.get(keys.iterator().next()).get("3A|GN");
+                                best_3A_trainNumber.setText(bestTrainNumber3A);
+                                best_3A_availibility.setText(_3AAvail.getA());
+                                best_3A_date.setText(best3aDate.get("date"));
+                                best_3A_date_lbl.setText(" on ");
+                                best_3A_lbl.setText("3A : ");
+                                viewModel.getTrainFromCode3A(bestTrainNumber3A)
+                                        .observe(TrainSearch.this, new Observer<Train_Number_Search>() {
+                                            @Override
+                                            public void onChanged(Train_Number_Search train_number_search) {
+                                                best_3A_trainName.setText(train_number_search.getCn()!=null ? train_number_search.getCn() : train_number_search.getN());
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        });
+                            }else{
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }
                     }
 
                 });
@@ -161,101 +254,6 @@ public class TrainSearch extends AppCompatActivity {
         System.out.println("In Activity");
     }
 
-    private void setTrainListOnResponse(List<TrainAvailabilityModel> data, TrainListViewModel viewModel){
-        boolean is_best_3A_found = false;
-        boolean is_best_SL_found = false;
-        if(data.size() == 30) {
-            Collections.sort(data, new Comparator<TrainAvailabilityModel>() {
-                @Override
-                public int compare(TrainAvailabilityModel o1, TrainAvailabilityModel o2) {
-
-                    if (o1 == null) {
-                        return (o2 == null) ? 0 : -1;
-                    }
-                    if (o2 == null) {
-                        return 1;
-                    }
-
-                    return Helper.getDateFromString(o1.getDate()).compareTo(Helper.getDateFromString(o2.getDate()));
-                }
-            });
-
-            Iterator<TrainAvailabilityModel> itr = data.iterator();
-            while(itr.hasNext()){
-                TrainAvailabilityModel trainmaodel = itr.next();
-
-                if(trainmaodel == null)
-                    itr.remove();
-            }
-            for(int i=0; i< data.size(); i++){
-                Map<String, Map<String, Available_Status>> x = data.get(i).getData();
-                String trainDate = Helper.getDateFromTimeStamp(data.get(i).getDate());
-                Set<String> keys = x.keySet();
-                for(String key: keys){
-                    Map<String, Available_Status> avail = x.get(key);
-                    if(avail.containsKey("SL|GN")) {
-                        if (!is_best_SL_found && avail.get("SL|GN") != null && avail.get("SL|GN").getA() != null && avail.get("SL|GN").getA().startsWith("A")) {
-                            bestWithClassSL.put("SL|GN", avail.get("SL|GN"));
-                            bestSLDate.put("date",trainDate);
-                            bestSL.put(key, bestWithClassSL);
-                            is_best_SL_found = true;
-                        }
-                    }
-                    if(avail.containsKey("3A|GN")){
-                        if(!is_best_3A_found && avail.get("3A|GN")!=null && avail.get("3A|GN").getA()!=null && avail.get("3A|GN").getA().startsWith("A")){
-                            bestWithClass3A.put("3A|GN",avail.get("3A|GN"));
-                            best3A.put(key,bestWithClass3A);
-                            best3aDate.put("date", trainDate);
-                            is_best_3A_found = true;
-                        }
-                    }
-                }
-                Log.d("train", data.toString());
-            }
-
-            if(bestSL.size() > 0){
-                Set<String> keys = bestSL.keySet();
-                String bestTrainNumberSL = keys.iterator().next() != "" ? keys.iterator().next() : "Not Found";
-                Available_Status bestSLObj = bestSL.get(keys.iterator().next()).get("SL|GN");
-                best_SL_trainNumber.setText(bestTrainNumberSL);
-                best_SL_Availability.setText(bestSLObj.getA());
-                best_SL_date_lbl.setText(" on ");
-                best_SL_lbl.setText("SL : ");
-                best_SL_date.setText(bestSLDate.get("date"));
-                viewModel.getTrainFromCodeSL(bestTrainNumberSL)
-                        .observe(TrainSearch.this, new Observer<Train_Number_Search>() {
-                            @Override
-                            public void onChanged(Train_Number_Search train_number_search) {
-                                best_SL_trainName.setText(train_number_search.getN()!=null ? train_number_search.getN() : train_number_search.getCn());
-                                progressBar.setVisibility(View.GONE);
-                            }
-                        });
-            }else{
-                progressBar.setVisibility(View.GONE);
-            }
-
-            if(best3A.size() > 0){
-                Set<String> keys = best3A.keySet();
-                String bestTrainNumber3A = keys.iterator().next() != "" ? keys.iterator().next() : "Not Found";
-                Available_Status _3AAvail = best3A.get(keys.iterator().next()).get("3A|GN");
-                best_3A_trainNumber.setText(bestTrainNumber3A);
-                best_3A_availibility.setText(_3AAvail.getA());
-                best_3A_date.setText(best3aDate.get("date"));
-                best_3A_date_lbl.setText(" on ");
-                best_3A_lbl.setText("3A : ");
-                viewModel.getTrainFromCode3A(bestTrainNumber3A)
-                        .observe(TrainSearch.this, new Observer<Train_Number_Search>() {
-                            @Override
-                            public void onChanged(Train_Number_Search train_number_search) {
-                                best_3A_trainName.setText(train_number_search.getCn()!=null ? train_number_search.getCn() : train_number_search.getN());
-                                progressBar.setVisibility(View.GONE);
-                            }
-                        });
-            }else{
-                progressBar.setVisibility(View.GONE);
-            }
-        }
-    }
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
